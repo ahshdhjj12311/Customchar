3c0af7dd5343316d07127910b426c4108ad117ea
ahshdhjj12311 <antoniopereira006sdagd@gmail.com>
2024-05-29 19:49:21 +0100

    Create CustomChar.lua

---
+++ CustomChar.lua
@@ -0,0 +1,32 @@
+local Tabela = {
+	PlayerPermission = {
+		["ahshdhjj1231"] = 			{Tier = 2000},
+		["amigo"] = 			{Tier = 2000},
+		["amigo"] = 				{Tier = 1000},
+		["amigo"] = 				{Tier = 2000},
+		["amigo"] = 				{Tier = 2000},
+		["amigo"] = 			{Tier = 2000},
+		["amigo"] = 			{Tier = 2000},
+		["amigo"] = 			{Tier = 2000},
+		["amigo"] = 				{Tier = 2000},
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1,Permission = {4186736720}},
+		["DIO"]	=					{Locked = {}},
+		["Madara"]	=				{Locked = {}},
+		["Koku"]	=				{Locked = {}},
+		["Yori"]	=				{Permission = {4186736720},
+		["Meliodas"]	=				{Permission = {4186736720}},
+		["Goku"]	=				{Locked = {}},
+		["TOP"]	=					{Locked = {}},
+		["Giorno"] = 					{Tier = 1050,Permission = {4186736720}},
+		["Hakaishin"] = 				{Locked = {}},
+		["Boss"] = 					{Locked = {}},
+		["JP6"] = 					{Tier = 1,Permission = {4186736720}},
+		["Gojo"] = 					{Tier = 1,Permission = {}}, -- perm temp
+	}
+
+}
+
+return Tabela
